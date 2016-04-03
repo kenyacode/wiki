b0e5398945d877d1d10d611823c5615fbffe0dd0
Joshua Munene <kenyadevelop@gmail.com>
2016-04-03 11:04:53 -0700

    Add article model & controller, index & show views. Bootstrap not working

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show, :edit, :update, :delete]
+	def index
+	end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(params[:article].permit(:title, :content))
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+		def find_article
+			@article = Article.find(params[:id])
+		end
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
